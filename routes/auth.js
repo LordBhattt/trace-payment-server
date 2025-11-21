2828c771597a8b7656f0b359150e16504fa9b3d7
LordBhattt <harsh.bhatt23@spit.ac.in>
2025-11-21 21:57:29 +0530

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+// SIGNUP
+router.post('/signup', async (req, res) => {
+  try {
+    const { name, email, phone, password } = req.body;
+
+    const exists = await User.findOne({ email });
+    if (exists)
+      return res.json({ success: false, message: "Email already in use" });
+
+    const user = await User.create({ name, email, phone, password });
+
+    return res.json({ success: true, message: "User registered" });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+
+// LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user)
+      return res.json({ success: false, message: "Invalid credentials" });
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match)
+      return res.json({ success: false, message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+    return res.json({
+      success: true,
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        profilePicture: user.profilePicture
+      }
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Server error" });
+  }
+});
+
+module.exports = router;
